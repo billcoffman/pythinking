1cdef97fb372ba37c46c5dce8cb399a758ae7576
Will Coffman <68365967+billcoffman@users.noreply.github.com>
2023-04-08 09:02:20 -0700

    Create get_merges_per_branch.py

---
+++ get_merges_per_branch.py
@@ -0,0 +1,37 @@
+import requests
+
+# Replace these with your values
+owner = 'your-repo-owner'
+repo = 'your-repo-name'
+token = 'your-personal-access-token'
+
+# Fetch default branch
+url = f'https://api.github.com/repos/{owner}/{repo}'
+headers = {'Authorization': f'token {token}'}
+response = requests.get(url, headers=headers)
+response.raise_for_status()
+default_branch = response.json()['default_branch']
+
+# Fetch all branches
+url = f'https://api.github.com/repos/{owner}/{repo}/branches'
+params = {'per_page': 100}
+response = requests.get(url, headers=headers, params=params)
+response.raise_for_status()
+branches = response.json()
+
+# Find merged branches
+merged_branches = []
+for branch in branches:
+    branch_name = branch['name']
+    if branch_name == default_branch:
+        continue
+
+    url = f'https://api.github.com/repos/{owner}/{repo}/branches/{branch_name}'
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    if response.json().get('merged'):
+        merged_branches.append(branch_name)
+
+print("Merged branches:")
+for branch in merged_branches:
+    print(branch)
