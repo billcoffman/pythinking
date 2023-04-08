fee65c5ff1db7713e49337433a4133665c599229
Will Coffman <68365967+billcoffman@users.noreply.github.com>
2023-04-08 08:48:20 -0700

    Update get_branches.py

--- get_branches.py
+++ get_branches.py
@@ -1,24 +1,37 @@
 import requests
 
 # Replace these with your values
+base_url = 'https://github.x.com/api/v3'  # Replace with your GitHub Enterprise base API URL
 owner = 'your-repo-owner'
 repo = 'your-repo-name'
 token = 'your-personal-access-token'
 
-# Fetch merged pull requests
-url = f'https://api.github.com/repos/{owner}/{repo}/pulls'
+# Fetch default branch
+url = f'{base_url}/repos/{owner}/{repo}'
 headers = {'Authorization': f'token {token}'}
-params = {'state': 'closed', 'per_page': 100}
+response = requests.get(url, headers=headers)
+response.raise_for_status()
+default_branch = response.json()['default_branch']
+
+# Fetch all branches
+url = f'{base_url}/repos/{owner}/{repo}/branches'
+params = {'per_page': 100}
 response = requests.get(url, headers=headers, params=params)
 response.raise_for_status()
-pulls = response.json()
+branches = response.json()
 
-# Extract merged branches
-merged_branches = set()
-for pull in pulls:
-    if pull.get('merged_at') is not None:
-        branch_name = pull['head']['ref']
-        merged_branches.add(branch_name)
+# Find merged branches
+merged_branches = []
+for branch in branches:
+    branch_name = branch['name']
+    if branch_name == default_branch:
+        continue
+
+    url = f'{base_url}/repos/{owner}/{repo}/branches/{branch_name}'
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    if response.json().get('merged'):
+        merged_branches.append(branch_name)
 
 print("Merged branches:")
 for branch in merged_branches:
