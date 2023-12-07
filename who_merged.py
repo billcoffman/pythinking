412aa08e1c80ed3afcbbc30cff913280d3c09ecb
Will Coffman <68365967+billcoffman@users.noreply.github.com>
2023-12-07 18:49:22 -0500

    Update who_merged.py
    admin ops

--- who_merged.py
+++ who_merged.py
@@ -1,13 +1,49 @@
-def get_pull_request_merger(pr):
-    if pr["merged_by"] is not None:
-        return pr["merged_by"]["login"]
-    return None
+import http.client
+import json
+import ssl
+from jwt import encode
+from datetime import datetime, timedelta
 
-# Usage example:
-pr = api.get_pull_request(owner, repo_name, pr_number)
-merger = get_pull_request_merger(pr)
-if merger:
-    print(f"Pull request merged by: {merger}")
-else:
-    print("Pull request not merged or merger information not available.")
+# Your GitHub Enterprise server and API endpoint
+server = "<your_github_enterprise_url>"
+endpoint = "/api/v3/repos/{owner}/{repo}"
+
+# Generating a JWT for authentication
+private_key = """-----BEGIN RSA PRIVATE KEY-----
+...
+-----END RSA PRIVATE KEY-----"""  # Your private key here
+
+# Prepare the payload for the JWT
+payload = {
+    'iat': datetime.utcnow(),
+    'exp': datetime.utcnow() + timedelta(minutes=10),
+    'iss': 'your_app_id'  # Your GitHub App's identifier
+}
+
+# Generate the JWT token
+encoded_jwt = encode(payload, private_key, algorithm='RS256')
+
+# Headers with the JWT token for authentication
+headers = {
+    'Authorization': f'Bearer {encoded_jwt}',
+    'User-Agent': 'Python http.client'
+}
+
+# Function to make a GET request
+def github_api_request(server, endpoint):
+    context = ssl._create_unverified_context()  # Only if SSL verification is not needed
+    connection = http.client.HTTPSConnection(server, context=context)
+    
+    try:
+        connection.request("GET", endpoint, headers=headers)
+        response = connection.getresponse()
+        return json.loads(response.read())
+    except Exception as e:
+        return {'error': str(e)}
+    finally:
+        connection.close()
+
+# Making an API call
+response = github_api_request(server, endpoint)
+print(json.dumps(response, indent=4))
 
